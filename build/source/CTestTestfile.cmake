5b850ecde626a2fa28e5ef86fd62427d604d6c87
arnaudsteen <arnaudsteen2.0@gmail.com>
2016-03-16 12:31:07 +0100

    rm build rep

--- build/source/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/arnaudsteen/CaloSoftWare/source
-# Build directory: /Users/arnaudsteen/CaloSoftWare/build/source
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
